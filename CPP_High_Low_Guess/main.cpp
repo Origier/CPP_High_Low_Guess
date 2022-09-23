79603d57bfeee05de41a3a289fbf7728cdc3bb80
Aaron McDonald <sergeantbeast456@gmail.com>
2022-09-23 07:34:15 -0400

    Add project files.

---
+++ CPP_High_Low_Guess/main.cpp
@@ -0,0 +1,15 @@
+// High Low Guessing Game
+// By: Aaron McDonald
+// This game generates a random number between 1 - 100 that the user must guess to try to get right
+// On a failed guess the game will generate a response such as, too high or too low depandent on 
+// how close the guess was to the number. A correct guess wins the game
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX_NUMBER_OF_GUESSES = 5;
+
+int main() {
+	return 0;
+}
